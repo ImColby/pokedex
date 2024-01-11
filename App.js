89a83f68f647b1043956b6f4cf1d23ad2ea5aba4
Colby Irizarry <Colby.irizarry@gmail.com>
2024-01-10 19:54:20 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBox from './SearchBox';
+import './App.css';
+
+
+function App() {
+  return (
+    <div> 
+      <h1>Your Online Pokedex</h1>
+        <SearchBox/>
+    </div>
+  )
+}
+
+export default App;
---
+++ SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBox() {
+    return (
+        <div>
+            <input type='search' placeholder='Search a Pokemon'/>
+            <button type='submit'>Submit</button>
+        </div>
+    )
+}
+
+export default SearchBox;
